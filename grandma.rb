d3222866abcba01382d70b4c795de0448784eb8c
Yonnick Brown <25hour24@gmail.com>
2019-01-26 18:29:44 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 
-def speak_to_grandma(say)
-  if say != say.upcase
+def speak_to_grandma(word)
+  if word != say.upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif say == "I LOVE YOU GRANDMA!"
+  elsif word == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "NO, NOT SINCE 1938!"
