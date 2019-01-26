773dd852bfac9e38895565391fcc2b225abace9e
Yonnick Brown <25hour24@gmail.com>
2019-01-26 18:21:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,8 +1,8 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(say)
-  if say != say.capitalize
-    puts "HUH?! SPEAK UP, SONNY!"
-  elsif say == "I LOVE YOU GRANDMA!"
+  # if say != say.capitalize
+  #   puts "HUH?! SPEAK UP, SONNY!"
+  if say == "I LOVE YOU GRANDMA!"
     puts "I LOVE YOU TOO PUMPKIN!"
   else
     puts "NO, NOT SINCE 1938!"
